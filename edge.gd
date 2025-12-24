c15a7ebdb3fd550e0da0721ee355611dab651344
katiikati <166103545+the-kat-kat@users.noreply.github.com>
2025-12-24 01:42:17 -0800

    add basic stick function

---
+++ edge.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+@export_enum("blue", "red", "green") var edge_color: String = "blue"
+@export var node_a: int = 0  # Start point 0
+@export var node_b: int = 1  # End point
+
+@onready var line = $Line2D
+@onready var collision = $CollisionShape2D
+
+func _ready():
+	add_to_group("edges")
+	setup()
+
+func setup():
+	if edge_color == "blue": 
+		line.default_color = Color.DODGER_BLUE
+	elif edge_color == "red": 
+		line.default_color = Color.CRIMSON
+	else: 
+		line.default_color = Color.PALE_GREEN
+	
+	if line.points.size() >= 2:
+		var shape = SegmentShape2D.new()
+		shape.a = line.points[0]
+		shape.b = line.points[1]
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	print_debug("heyy")
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		print("touched")
+		get_parent().handle_cut(self)
+
+
+func _on_mouse_entered() -> void:
+	print_debug("hi there")
---
+++ scenes/hackenbush.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+var graph = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for child in get_children():
+		if child.is_in_group("edges"):
+			register_edge(child)
+
+func register_edge(edge: Node2D):
+	graph[edge] = [edge.node_a, edge.node_b]
+	
+func handle_cut(edge_node: Node2D):
+	print("currting: ", edge_node.name)
+	graph.erase(edge_node)
+	edge_node.queue_free()
+	
+	await get_tree().process_frame
+	check_floating_edges()
+	
+func check_floating_edges():
+	var grounded = []
+	var stack = [0] # 0 is ground
+	
+	while stack.size()>0:
+		var current_node = stack.pop_back()
+		if current_node not in grounded:
+			grounded.append(current_node)
+			
+			for edge in graph.keys():
+				var nodes = graph[edge]
+				if current_node in nodes:
+					var next_node = nodes[1] if nodes[0] == current_node else nodes[0]
+					stack.append(next_node)
+					
+	for edge in graph.keys():
+		var nodes = graph[edge]
+		var is_safe = false
+		for n in nodes:
+			if n in grounded:
+				is_safe = true
+				
+			if not is_safe:
+				edge.queue_free()
+				graph.erase(edge)
+					
